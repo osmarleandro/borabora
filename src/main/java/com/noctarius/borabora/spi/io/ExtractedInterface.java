ced752487a25ee0c71ace07f991457e23aa0b93e
Osmar Leandro <osmarleandrods@gmail.com>
2020-03-06 21:34:07 -0300

    ref: Extract interface ExtractedInterface at BinaryProjectionStrategy with beginSelect()

---
+++ src/main/java/com/noctarius/borabora/spi/io/ExtractedInterface.java
@@ -0,0 +1,9 @@
+package com.noctarius.borabora.spi.io;
+
+import com.noctarius.borabora.spi.query.QueryContext;
+
+public interface ExtractedInterface {
+
+	void beginDictionary(QueryContext queryContext);
+
+}
